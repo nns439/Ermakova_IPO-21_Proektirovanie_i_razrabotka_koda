11c5a52606cec660b7178743fe65db55f98b1ff1
nanse <nanse7nns@gmail.com>
2026-02-17 22:29:57 +0300

    Add files via upload

---
+++ Рандомайзер_изображений/RandomImageApp/RandomImageApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RandomImageApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(160, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(734, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonRandom.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRandom.Font = new System.Drawing.Font("Mongolian Baiti", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRandom.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonRandom.Location = new System.Drawing.Point(446, 461);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(145, 58);
+            this.buttonRandom.TabIndex = 1;
+            this.buttonRandom.Text = "Click";
+            this.buttonRandom.UseVisualStyleBackColor = false;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Azure;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(383, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Random image of a cat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1055, 542);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Рандомайзер_изображений/RandomImageApp/RandomImageApp/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RandomImageApp
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        string[] images;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string folderPath = Path.Combine(Application.StartupPath, "Images");
+
+            if (Directory.Exists(folderPath))
+            {
+                images = Directory.GetFiles(folderPath);
+            }
+            else
+            {
+                MessageBox.Show("Папка Images не найдена!");
+                images = new string[0];
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            if (images.Length == 0)
+            {
+                MessageBox.Show("Нет картинок!");
+                return;
+            }
+
+            int index = random.Next(images.Length);
+            pictureBox1.ImageLocation = images[index];
+        }
+    }
+}
